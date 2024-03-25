cdc3c4afa58196728276f98b85e8af6f2abde528
BenStephen23 <benstephe 2000@gmail.com>
2024-03-25 05:12:58 -0700

    intro to js

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+// console.log("hello world");
+// console.log(2+2);
+function addi (a, b) {
+    // return evaluates but doesn't print
+    // argumentas are the value of parameters
+    console.log(a + b); 
+}
+
+addi(5,9)
+
+function words (z, x) {
+    console.log(`I added 12 pieces of ${z} and 7 pieces of ${x}`);
+}
+words ("banana", "apple")
+
+// variables are the name of parameters used to store data. var-deprecated, let, const.
+
+// data types. means or method of storing data, two types, Primitive and non-primitive.
+// primitive consists of 6 data types, number, string, boolean, null, undefined, symbol. string-text data, number- numeric data, boolean- true or false, null- empty value, undefined- no value, symbol- unique value.
