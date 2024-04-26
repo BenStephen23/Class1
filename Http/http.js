65e5c002f6b733511f645b34db36a3ffe9386ee0
BenStephen23 <benstephe 2000@gmail.com>
2024-04-26 12:10:41 -0700

    fetch fixed

--- Http/http.js
+++ Http/http.js
@@ -12,40 +12,79 @@
 //     .catch (err => console.log("Error catching data using Axios"), err)
 // }
 
+// window.onload = function(){
+//     axios.get("https://restcountries.com/v3.1/name/name")
+//     .then (res => {
+//         let val =  res.data;
+//         console.log(val);
+//         let countryList = document.getElementById("country-list")
+//         let country = document.getElementById("country");
+//         let capital = document.getElementById("capital");
+//         let currency = document.getElementById("currency");
+//         let language = document.getElementById("language");
+//         country.textContent = val[0].name.common
+//         capital.textContent = val[0].capital;
+//         currency.textContent = val[0].currencies.SRD.name;
+//         language.textContent = val[0].languages.nld 
 
-window.onload = function(){
-    axios.get("https://restcountries.com/v3.1/name/name")
-   .then(res => {(res.data[0]);
-    const val = res.data[0];
-    console.log(val)
-    let countryList = document.getElementById("country-list");
-    let country = document.getElementById("country");
-    let capital = document.getElementById("capital");
-    let currency = document.getElementById("currency");
-    let language = document.getElementById("language");
+//         countryList.style.width = "100%"
+//         countryList.style.borderWidth = "collapse"
+//         countryList.style.textAlign = "center"
+//         let rows = document.querySelectorAll("#country-list tr")
+//         rows.forEach(function(row) {
+//             row.style.width = "100%"
+//             row.style.borderWidth = "collapse"
+//             row.style.textAlign = "center"
+//             row.style.backgroundColor = "lightblue"
+//         })
+//     }).catch (err => {
+//             alert("Error fetching code using Axios")
+//         err})
 
-    country.textContent = val.name.common;
-    capital.textContent = val.capital;
-    currency.textContent = val.currencies.SRD.name;
-    language.textContent = val.languages.nld
+// }
 
-    countryList.style.width = "100%"
-    countryList.style.borderWidth = "collapse"
-    countryList.style.textAlign = "center"
 
-    let rows = document.querySelectorAll("#country-list tr")
-    rows.forEach(function(row) {
-        row.style.width = "100%"
-        row.style.borderWidth = "collapse"
-        row.style.textAlign = "center"
-        row.style.backgroundColor = "lightblue"
+// fetch method
+
+fetch("https://restcountries.com/v3.1/name/name") 
+
+// .then(res => res.json()) 
+// .then(val => {
+//     console.log(val);
+// });
+// or 
+
+    .then(function (res){
+        return ( res.json())
     })
-})
+    .then (function (val){
+        console.log(val);
+        let countryList = document.getElementById("country-list")
+        let country = document.getElementById("country");
+        let capital = document.getElementById("capital");
+        let currency = document.getElementById("currency");
+        let language = document.getElementById("language");
+        country.textContent = val[0].name.common;
+        capital.textContent = val[0].capital;
+        currency.textContent = val[0].currencies.SRD.name;
+        language.textContent = val[0].languages.nld
+
+        countryList.style.width = "100%"
+        countryList.style.borderWidth = "collapse"
+        countryList.style.textAlign = "center"
+        let rows = document.querySelectorAll("#country-list tr")
+        rows.forEach(function(row) {
+            row.style.width = "100%"
+            row.style.borderWidth = "collapse"
+            row.style.textAlign = "center"
+            row.style.backgroundColor = "lightblue"
+        })
+    })
 .catch (err => {
-    alert("Error fetching code using Axios")
+    alert("Error fetching code using Fetch")
 err})
     
 
 
 
-}
+
