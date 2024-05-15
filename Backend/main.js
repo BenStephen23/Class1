b3ae000276417e3eb766d30c98bd6c3f5b6990d7
BenStephen23 <benstephe 2000@gmail.com>
2024-05-15 04:07:48 -0700

    server

--- Backend/main.js
+++ Backend/main.js
@@ -32,29 +32,89 @@
 
 // console.log("Program Ended");
 
+
+
 // Import events module
-var events = require('events');
+// var events = require('events');
 
-// Create an eventEmitter object
-var eventEmitter = new events.EventEmitter();
+// // Create an eventEmitter object
+// var eventEmitter = new events.EventEmitter();
 
-// Create an event handler as follows
-var connectHandler = function connected() {
-   console.log('connection succesful.');
+// // Create an event handler as follows
+// var connectHandler = function connected() {
+//    console.log('connection succesful.');
   
-   // Fire the data_received event 
-   eventEmitter.emit('data_received');
-}
+//    // Fire the data_received event 
+//    eventEmitter.emit('data_received');
+// }
 
-// Bind the connection event with the handler
-eventEmitter.on('connection', connectHandler);
+// // Bind the connection event with the handler
+// eventEmitter.on('connection', connectHandler);
  
-// Bind the data_received event with the anonymous function
-eventEmitter.on('data_received', function() {
-   console.log('data received succesfully.');
+// // Bind the data_received event with the anonymous function
+// eventEmitter.on('data_received', function() {
+//    console.log('data received succesfully.');
+// });
+
+// // Fire the connection event 
+// eventEmitter.emit('connection');
+
+// console.log("Program Ended.");
+
+
+// var events = require('events');
+// var eventEmitter = new events.EventEmitter();
+
+// // listener #1
+// var listner1 = function listner1() {
+//    console.log('listner1 executed.');
+// }
+
+// // listener #2
+// var listner2 = function listner2() {
+//    console.log('listner2 executed.');
+// }
+
+// // Bind the connection event with the listner1 function
+// eventEmitter.addListener('connection', listner1);
+
+// // Bind the connection event with the listner2 function
+// eventEmitter.on('connection', listner2);
+
+// var eventListeners = require('events').EventEmitter.listenerCount
+//    (eventEmitter,'connection');
+// console.log(eventListeners + " Listner(s) listening to connection event");
+
+// // Fire the connection event 
+// eventEmitter.emit('connection');
+
+// // Remove the binding of listner1 function
+// eventEmitter.removeListener('connection', listner1);
+// console.log("Listner1 will not listen now.");
+
+// // Fire the connection event 
+// eventEmitter.emit('connection');
+
+// eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+// console.log(eventListeners + " Listner(s) listening to connection event");
+
+// console.log("Program Ended.");
+
+var fs = require("fs");
+
+console.log("Going to write into existing file");
+fs.writeFile('input.txt', 'Simply Easy Learning!', function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   
+   console.log("Data written successfully!");
+   console.log("Let's read newly written data");
+   
+   fs.readFile('input.txt', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      console.log("Asynchronous read: " + data.toString());
+   });
 });
-
-// Fire the connection event 
-eventEmitter.emit('connection');
-
-console.log("Program Ended.");
